2b1fd514e724b41b70e49f0632aa0ab05ec09c7a
Christopher Hebert <hebert.christopherj@gmail.com>
2018-06-14 22:10:54 -0700

    unfunctional changes to incorporate bar chart

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -290,12 +290,14 @@ var file_selected = function() {
     SelectedFileName.innerHTML = 'Name: ' + FileUpload.files[0].name;
 }
 
-var update_bar_chart = function(csv_text) {
-    console.log(csv_text);
+var update_bar_chart = function(db_entry) {
     var width = 1950;
-    var parsed_points = Papa.parse(csv_text, {header: true}).data;
-    var data_points = parsed_points.map(function(data) { return {label: data.id, value: data.value}; });
-    var data_points = data_points.filter(function(data) { return data.value; });
+    var parsed_points = [];
+    for (var word in db_entry) {
+	parsed_points.push({label: word, value: db_entry[word]});
+    }
+    //var data_points = parsed_points.map(function(data) { return {label: data.id, value: data.value}; });
+    var data_points = parsed_points.filter(function(data) { return data.value; });
     data_points.sort(function (a, b) { return b.value - a.value; });
 
     draw_rect(rect(0, 0, Canvas.width, Canvas.height), BgColor, false);
@@ -305,13 +307,13 @@ var update_bar_chart = function(csv_text) {
 
 var CurrentCSVText;
 
-
 angular.module('userApp.controllers', [])
-    .controller("myCtrl", function ($scope) {
+    .controller("myCtrl", function ($scope, fileService) {
 	Canvas = document.getElementById('canvas');
 	setup_canvas_context();
 
-       getData();
+	getData();
+
         $scope.name = '';
         $scope.fileName = null;
         $scope.files = [];
@@ -340,151 +342,124 @@ angular.module('userApp.controllers', [])
         var flag = false;
         $scope.updateVisual = function (data) {
             console.log($scope.files);
+	    console.log(data)
+	    $scope.selectedFile = data;
             // var parent = document.getElementById("main");
             if (flag) {
                 document.getElementById("svg_id").innerHTML = '';
             }
-            $scope.selectedFile = data;
-            var file = 'csv/' + data + $scope.selectedType + '.csv';
-            loadVisual(file);
-	    d3.request(file).mimeType('text/csv').get(function (xhr) {
-		var csv_text = xhr.responseText;
-		CurrentCSVText = csv_text;
-		update_bar_chart(csv_text);
-	    });
+	    if ($scope.selectedType === 'Words') {
+		update_bar_chart(data.words);
+		loadVisual(data.words);
+	    } else {
+		update_bar_chart(data.bigrams);
+		loadVisual(data.bigrams);
+	    }
             flag = true;
 
         }
 
-        function loadVisual(file) {
-            if ($scope.chart === 'bubble') {
+        function loadVisual(data) {
+	    var classes = [];
+	    for (var d in data) {
+		classes.push({id: d, value: data[d]});
+	    }
 
 
-                var svg = d3.select("svg").attr("id", "svg_id"),
-                    width = +svg.attr("width"),
-                    height = +svg.attr("height");
+            var svg = d3.select("svg").attr("id", "svg_id"),
+                width = +svg.attr("width"),
+                height = +svg.attr("height");
 
-                var format = d3.format(",d");
+            var format = d3.format(",d");
 
-                var color = d3.scaleOrdinal(d3.schemeCategory20c);
+            var color = d3.scaleOrdinal(d3.schemeCategory20c);
 
-                var pack = d3.pack()
-                    .size([width, height])
-                    .padding(1.5);
+            var pack = d3.pack()
+                .size([width, height])
+                .padding(1.5);
 
-                d3.csv(file, function (d) {
-                    d.value = +d.value;
-                    if (d.value) return d;
-                }, function (error, classes) {
-                    if (error) throw error;
 
-                    var root = d3.hierarchy({
-                            children: classes
-                        })
-                        .sum(function (d) {
-                            return d.value;
-                        })
-                        .each(function (d) {
-                            if (id = d.data.id) {
-                                var id, i = id.lastIndexOf(".");
-                                d.id = id;
-                                d.package = id.slice(0, i);
-                                d.class = id.slice(i + 1);
-                            }
-                        });
-
-                    var node = svg.selectAll(".node")
-                        .data(pack(root).leaves())
-                        .enter().append("g")
-                        .attr("class", "node")
-                        .attr("transform", function (d) {
-                            return "translate(" + d.x + "," + d.y + ")";
-                        });
-                    node.attr("class", "remove");
-
-                    // makes circles
-                    node.append("circle")
-                        .attr("id", function (d) {
-                            return d.id;
-                        })
-                        .attr("value", function (d) {
-                            return d.value;
-                        })
-                        .attr("r", function (d) {
-                            return d.r;
-                        })
-                        .style("fill", function (d) {
-                            return color(d.package);
-                        });
-
-                    // prevents text overflow
-                    node.append("clipPath")
-                        .attr("id", function (d) {
-                            return "clip-" + d.id;
-                        })
-                        .append("use")
-                        .attr("xlink:href", function (d) {
-                            return "#" + d.id;
-                        });
-                    node.append("title")
-                        .text(function (d) {
-                            return d.id + "\n" + format(d.value);
-                        });
-                    node.append("text")
-                        .attr("clip-path", function (d) {
-                            return "url(#clip-" + d.id + d.value + ")";
-                        })
-                        .attr("class", "enter")
-                        .selectAll("tspan")
-                        .data(function (d) {
-                            return d.class.split(/(?=[A-Z][^A-Z])/g);
-                        })
-                        .enter().append("tspan")
-                        .attr("x", 0)
-                        .attr("y", function (d, i, nodes) {
-                            return 13 + (i - nodes.length / 2 - 0.5) * 10;
-                        })
-                        .merge(node)
-                        .text(function (d) {
-                            return d;
-                        });
-                    node.exit().remove();
-
+            var root = d3.hierarchy({
+                children: classes
+            })
+                .sum(function (d) {
+                    return d.value;
+                })
+                .each(function (d) {
+                    if (id = d.data.id) {
+                        var id, i = id.lastIndexOf(".");
+                        d.id = id;
+                        d.package = id.slice(0, i);
+                        d.class = id.slice(i + 1);
+                    }
                 });
 
+            var node = svg.selectAll(".node")
+                .data(pack(root).leaves())
+                .enter().append("g")
+                .attr("class", "node")
+                .attr("transform", function (d) {
+                    return "translate(" + d.x + "," + d.y + ")";
+                });
+            node.attr("class", "remove");
 
-            } else {
-                const data = ["I", "II", "III"];
+            // makes circles
+            node.append("circle")
+                .attr("id", function (d) {
+                    return d.id;
+                })
+                .attr("value", function (d) {
+                    return d.value;
+                })
+                .attr("r", function (d) {
+                    return d.r;
+                })
+                .style("fill", function (d) {
+                    return color(d.package);
+                });
 
-                const nameScale = d3.scaleOrdinal()
-                  .domain(data)
-                  .range(["Jan", "Feb", "Mar"]);
-              
-                const widthScale = d3.scaleOrdinal()
-                  .domain(data)
-                  .range([300, 100, 900]);
-              
-                const color = d3.scaleOrdinal(d3.schemeCategory10);
-              
-                d3.select("body")
-                  .selectAll("div")
-                  .data(data)
-                  .enter()
-                  .append("div")
-                  .text(nameScale)
-                  .style("width", d => widthScale(d) + "px")
-                  .style("background-color", color);
-            }
+            // prevents text overflow
+            node.append("clipPath")
+                .attr("id", function (d) {
+                    return "clip-" + d.id;
+                })
+                .append("use")
+                .attr("xlink:href", function (d) {
+                    return "#" + d.id;
+                });
+            node.append("title")
+                .text(function (d) {
+                    return d.id + "\n" + format(d.value);
+                });
+            node.append("text")
+                .attr("clip-path", function (d) {
+                    return "url(#clip-" + d.id + d.value + ")";
+                })
+                .attr("class", "enter")
+                .selectAll("tspan")
+                .data(function (d) {
+                    return d.class.split(/(?=[A-Z][^A-Z])/g);
+                })
+                .enter().append("tspan")
+                .attr("x", 0)
+                .attr("y", function (d, i, nodes) {
+                    return 13 + (i - nodes.length / 2 - 0.5) * 10;
+                })
+                .merge(node)
+                .text(function (d) {
+                    return d;
+                });
+            node.exit().remove();
         }
 
         function getData() {
             return new Promise((resolve, reject) => {
                 var data = fileService.get()
                 setTimeout(function() {
-                    console.log($scope.files);
+                    //console.log($scope.files);
                     $scope.files = data.value.data
                     // console.log(data.value.data);
-                    console.log($scope.files);
+                    //console.log($scope.files);
                     
                     resolve( data.value.data);
                 }, 800);
